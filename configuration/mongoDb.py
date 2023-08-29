87574a7d5e29681b54c9d5c6e52122806ff40799
Sunil Kachhawah <60739119+sunilkachhawah@users.noreply.github.com>
2023-08-29 21:06:38 +0530

    Update mongoDb.py

--- configuration/mongoDb.py
+++ configuration/mongoDb.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient, ASCENDING, IndexModel
 # Set up MongoDB connection
-client = MongoClient("mongodb+srv://root:root@cluster0.oni3uns.mongodb.net/")
+client = MongoClient("mongodb://localhost:27017")
 db = client["temp_otp_data"]
 collection = db["dumpdata"]
 # Create a TTL index for automatic deletion after 5 minutes
